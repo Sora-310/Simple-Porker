0f39f383db3e1e026d3b3567c1d7210e316e4a2b
Sora-310 <114633527+Sora-310@users.noreply.github.com>
2023-01-08 00:24:25 +0900

    Check encoding

--- jikken.java
+++ jikken.java
@@ -1,42 +1,43 @@
 import javax.swing.*;
-import java.awt.*;
+// import java.awt.*;
 // import java.awt.event.*;
 
 class jikken extends JFrame
 {
 	public static void main(String[] args)
 	{
-		jikken j = new jikken();
-		j.setSize(700, 400);
-		j.setTitle("実験");
-		j.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		j.setLocationRelativeTo(null);
-		j.setVisible(true);
+		// jikken j = new jikken();
+		// j.setSize(700, 400);
+		// j.setTitle("実験");
+		// j.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// j.setLocationRelativeTo(null);
+		// j.setVisible(true);
+		System.out.println(System.getProperty("file.encoding"));
 	}
 
 	// private ImageIcon icon;
 	// private JLabel lb;
-	private JButton bt;
+	// private JButton bt;
 
-	public jikken()
-	{
-		setLayout(null);
+	// public jikken()
+	// {
+	// 	setLayout(null);
 
-		bt = new JButton(imageResize(101));
-		bt.setBounds(100, 100, 90, 120);
-		add(bt);
-	}
+	// 	bt = new JButton(imageResize(101));
+	// 	bt.setBounds(100, 100, 90, 120);
+	// 	add(bt);
+	// }
 
-	public ImageIcon imageResize(int num)
-	{
-		// 画像を取得
-		ImageIcon icon = new ImageIcon("0ic_middle\\" + Integer.toString(num) + ".png");
+	// public ImageIcon imageResize(int num)
+	// {
+	// 	// 画像を取得
+	// 	ImageIcon icon = new ImageIcon("0ic_middle\\" + Integer.toString(num) + ".png");
 
-		// Imageクラスを用いてサイズを調整
-		Image img = icon.getImage();
-		Image newimg = img.getScaledInstance(90, 120, Image.SCALE_SMOOTH);
-		icon = new ImageIcon(newimg);
+	// 	// Imageクラスを用いてサイズを調整
+	// 	Image img = icon.getImage();
+	// 	Image newimg = img.getScaledInstance(90, 120, Image.SCALE_SMOOTH);
+	// 	icon = new ImageIcon(newimg);
 
-		return icon;
-	}
+	// 	return icon;
+	// }
 }
