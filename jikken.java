d408bf44f17f7046d5383b3bf239911196b64051
Sora-310 <114633527+Sora-310@users.noreply.github.com>
2023-01-10 00:55:05 +0900

    Experiment of sound

--- jikken.java
+++ jikken.java
@@ -1,32 +1,44 @@
 import javax.swing.*;
 // import java.awt.*;
-// import java.awt.event.*;
+import java.awt.event.*;
+
+import java.applet.*;
 
 class jikken extends JFrame
 {
 	public static void main(String[] args)
 	{
-		// jikken j = new jikken();
-		// j.setSize(700, 400);
-		// j.setTitle("実験");
-		// j.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// j.setLocationRelativeTo(null);
-		// j.setVisible(true);
-		System.out.println(System.getProperty("file.encoding"));
+		jikken j = new jikken();
+		j.setSize(700, 400);
+		j.setTitle("実験");
+		j.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		j.setLocationRelativeTo(null);
+		j.setVisible(true);
 	}
 
 	// private ImageIcon icon;
 	// private JLabel lb;
-	// private JButton bt;
+	private JButton bt;
+	private AudioClip ac;
 
-	// public jikken()
-	// {
-	// 	setLayout(null);
+	public jikken()
+	{
+		setLayout(null);
 
-	// 	bt = new JButton(imageResize(101));
-	// 	bt.setBounds(100, 100, 90, 120);
-	// 	add(bt);
-	// }
+		bt = new JButton("再生");
+		bt.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				ac.play();
+			}
+		});
+		bt.setBounds(100, 100, 90, 120);
+		add(bt);
+
+		ac = Applet.newAudioClip(getClass().getResource("sss.wav"));
+	}
 
 	// public ImageIcon imageResize(int num)
 	// {
