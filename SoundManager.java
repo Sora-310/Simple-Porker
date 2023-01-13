9adb2579127ce35af5ab848589be3ce81d8fd019
Sora-310 <114633527+Sora-310@users.noreply.github.com>
2023-01-13 16:26:44 +0900

    Create class

---
+++ SoundManager.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import javax.sound.sampled.*;
+
+class SoundManager {
+	public static Clip createClip(File path)
+	{
+		//指定されたURLのオーディオ入力ストリームを取得
+		try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
+			
+			//ファイルの形式取得
+			AudioFormat af = ais.getFormat();
+			
+			//単一のオーディオ形式を含む指定した情報からデータラインの情報オブジェクトを構築
+			DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
+			
+			//指定された Line.Info オブジェクトの記述に一致するラインを取得
+			Clip c = (Clip)AudioSystem.getLine(dataLine);
+			
+			//再生準備完了
+			c.open(ais);
+			
+			return c;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public static void playback(String path)
+	{
+		Clip clip = createClip(new File(path));
+		clip.start();
+	}
+}
