7eb3946c6b11876b3e8ff7ce8e2318c98bb70333
Sora-310 <114633527+Sora-310@users.noreply.github.com>
2023-01-16 01:55:24 +0900

    Code Changes and Additions

--- PlayPanel.java
+++ PlayPanel.java
@@ -40,6 +40,14 @@ class PlayPanel extends JPanel
 		Image img = arrowIcon.getImage();
 		Image newimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
 		arrowIcon = new ImageIcon(newimg);
+		
+		// 矢印用のJLabelを5枚用意し、設置
+		for(int i = 0; i < 5; i++)
+		{
+			arrow[i] = new JLabel();
+			arrow[i].setBounds((120 * i) + 55, 195, 100, 100);
+			add(arrow[i]);
+		}
 
 		// 選択判断用配列を初期化
 		for(int i = 0; i < 5; i++)
@@ -77,84 +85,7 @@ class PlayPanel extends JPanel
 
 		// 判定ボタン
 		judgeButton = new JButton("ＯＫ");
-		judgeButton.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				SoundManager.playback(select_sou);
-				if(isJudge)
-				{
-					for(int i = 0; i < 5; i++)
-					{
-						// 選択していない時、カードを交換
-						if(!choise[i])
-						{
-							remove(image[i]);
-							repaint();
-							
-							// カードを交換して表示
-							cards5[i] = cards.drowCard(i + 7);
-							image[i] = makeCard(i);
-							add(image[i]);
-						}
-						else	// そうでない時、矢印を消す
-						{
-							choise[i] = false;
-							setArrow(i, false);
-						}
-					}
-
-					// 判定を表示
-					int judge_num = Porker.judgeHand(cards5);
-					if(judge_num > 0 && judge_num < 7)	// ストレート以上の時、音を鳴らす
-					{
-						SoundManager.playback(fanfare_sou);
-					}
-					sentence.setText(Porker.getStringHand(judge_num));
-
-					// ボタンの文を変更
-					judgeButton.setText("再挑戦");
-
-					// ボタンの機能を切り替え
-					isJudge = false;
-				}
-				else
-				{
-					// 表示されているカードを削除
-					for(int i = 0; i < 5; i++)
-					{
-						cards5[i] = 0;
-						remove(image[i]);
-					}
-					repaint();
-
-					// 選択判断用配列を初期化
-					for(int i = 0; i < 5; i++)
-					{
-						choise[i] = false;
-						setArrow(i, false);
-					}
-
-					// 文を元に戻す
-					sentence.setText("ホールドするカードを選択してください");
-
-					// 引き直したカードを表示
-					cards.drowHand(cards5);
-					for(int i = 0; i < 5; i++)
-					{
-						image[i] = makeCard(i);
-						add(image[i]);
-					}
-
-					// ボタンの文を変更
-					judgeButton.setText("ＯＫ");
-
-					// ボタンの機能を切り替え
-					isJudge = true;
-				}
-			}
-		});
+		judgeButton.addActionListener(new JudgeActionListener());
 		judgeButton.setBounds(275, 280, 150, 70);
 		add(judgeButton);
 
@@ -206,27 +137,10 @@ class PlayPanel extends JPanel
 			image[i] = makeCard(i);
 			add(image[i]);
 		}
-
-		// 矢印5個（最初は非表示）
-		for(int i = 0; i < 5; i++)
-		{
-			arrow[i] = new JLabel();
-			arrow[i].setBounds((120 * i) + 55, 195, 100, 100);
-			add(arrow[i]);
-		}
 	}
 
 	// カードを1枚表示
 	private JButton makeCard(int num)
-	{
-		JButton button = new JButton(imageResize(num));
-		button.addActionListener(new CardActionListener());
-		button.setBounds((120 * num) + 60, 105, 90, 120);
-		return button;
-	}
-
-	// カードの画像のサイズを調節し、返す
-	private ImageIcon imageResize(int num)
 	{
 		// 画像を取得
 		ImageIcon icon = new ImageIcon("images\\" + Integer.toString(cards5[num]) + ".png");
@@ -235,8 +149,12 @@ class PlayPanel extends JPanel
 		Image img = icon.getImage();
 		Image newimg = img.getScaledInstance(90, 120, Image.SCALE_SMOOTH);
 		icon = new ImageIcon(newimg);
-		
-		return icon;
+
+		// カードを定義
+		JButton button = new JButton(icon);
+		button.addActionListener(new CardActionListener());
+		button.setBounds((120 * num) + 60, 105, 90, 120);
+		return button;
 	}
 
 	// 矢印の表示を更新
@@ -262,8 +180,6 @@ class PlayPanel extends JPanel
 	// トランプを選択した時の動作
 	class CardActionListener implements ActionListener
 	{
-		private Clip select_sou = SoundManager.createClip(new File("sounds\\select_sou.wav"));
-		
 		public void actionPerformed(ActionEvent e)
 		{
 			SoundManager.playback(select_sou);
@@ -286,6 +202,84 @@ class PlayPanel extends JPanel
 
 					break;
 				}
+			}
+		}
+	}
+
+	class JudgeActionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			SoundManager.playback(select_sou);
+			if(isJudge)
+			{
+				for(int i = 0; i < 5; i++)
+				{
+					// 選択していない時、カードを交換
+					if(!choise[i])
+					{
+						remove(image[i]);
+						repaint();
+						
+						// カードを交換して表示
+						cards5[i] = cards.drowCard(i + 7);
+						image[i] = makeCard(i);
+						add(image[i]);
+					}
+					else	// そうでない時、矢印を消す
+					{
+						choise[i] = false;
+						setArrow(i, false);
+					}
+				}
+
+				// 判定を表示
+				int judge_num = Porker.judgeHand(cards5);
+				if(judge_num > 0 && judge_num < 7)	// ストレート以上の時、音を鳴らす
+				{
+					SoundManager.playback(fanfare_sou);
+				}
+				sentence.setText(Porker.getStringHand(judge_num));
+
+				// ボタンの文を変更
+				judgeButton.setText("再挑戦");
+
+				// ボタンの機能を切り替え
+				isJudge = false;
+			}
+			else
+			{
+				// 表示されているカードを削除
+				for(int i = 0; i < 5; i++)
+				{
+					cards5[i] = 0;
+					remove(image[i]);
+				}
+				repaint();
+
+				// 選択判断用配列を初期化
+				for(int i = 0; i < 5; i++)
+				{
+					choise[i] = false;
+					setArrow(i, false);
+				}
+
+				// 文を元に戻す
+				sentence.setText("ホールドするカードを選択してください");
+
+				// 引き直したカードを表示
+				cards.drowHand(cards5);
+				for(int i = 0; i < 5; i++)
+				{
+					image[i] = makeCard(i);
+					add(image[i]);
+				}
+
+				// ボタンの文を変更
+				judgeButton.setText("ＯＫ");
+
+				// ボタンの機能を切り替え
+				isJudge = true;
 			}
 		}
 	}
